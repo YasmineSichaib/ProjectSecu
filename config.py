ce88547227072425dbc5ba6a9d64390ffb2824ea
YasmineSichaib <106628276+YasmineSichaib@users.noreply.github.com>
2022-06-01 00:36:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-# Edit this config.py file as you like
-
-# This is hastag which Twitter bot will
-# search and retweet You can edit this with
-# any hastag .For example : '# javascript'
 
 QUERY = ['#cybersecurity']
 
@@ -12,10 +7,6 @@ LIKE = True
 # Twitter bot setting for following user who tweeted
 FOLLOW = True
 
-# Twitter bot sleep time settings in seconds.
-# For example SLEEP_TIME = 300 means 5 minutes.
-# Please, use large delay if you are running bot
-# all the time so that your account does not
-# get banned.
+
 
 SLEEP_TIME = 10
