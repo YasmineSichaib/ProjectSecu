4dab84ebaa3435c419f176bc93201ecef33ddd0b
YasmineSichaib <106628276+YasmineSichaib@users.noreply.github.com>
2022-06-01 00:37:00 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,3 @@
-# This is just a sample file. You need to
-# edit this file. You need to get these
-# details from your Twitter app settings.
-
 consumer_key = 'vdz0J5ges1nKhJwbGk2gwamIc'
 consumer_secret = 'oi5ukUVHaRqoDr9PHSCMuklKtBU8w2l1AEQMAcHpW2sVCw6kxR'
 access_token = '1526495086908948480-yLBDwCwxuczjqZ3cb8MPaJp6oGfYbH'
