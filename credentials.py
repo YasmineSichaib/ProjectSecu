feea05ce2778a08c0759d8adfe4b8332707c4816
YasmineSichaib <106628276+YasmineSichaib@users.noreply.github.com>
2022-06-01 00:17:05 +0100

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# This is just a sample file. You need to
+# edit this file. You need to get these
+# details from your Twitter app settings.
+
+consumer_key = 'vdz0J5ges1nKhJwbGk2gwamIc'
+consumer_secret = 'oi5ukUVHaRqoDr9PHSCMuklKtBU8w2l1AEQMAcHpW2sVCw6kxR'
+access_token = '1526495086908948480-yLBDwCwxuczjqZ3cb8MPaJp6oGfYbH'
+access_token_secret = 'EqfrEg0x7EaNIxFa5s8zc4J1NMb6H96UxE3tLg9q43sAf'
---
+++ twitterbot_retweet.py
@@ -0,0 +1,60 @@
+import tweepy
+from time import sleep
+from credentials import *
+from config import QUERY, FOLLOW, LIKE, SLEEP_TIME
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+tweets = api.mentions_timeline()
+twts = api.search_tweets(q="Hello World!", count=1 , lang="en" , result_type="recent")
+#Vérifier l'état de la connexion
+try:
+    api.verify_credentials()
+    print('Vous êtes connecté!')
+except: 
+    print("Erreur de connexion.")
+
+#Afficher mes tweets
+print("Vos tweets : ")
+for tweet in tweets:
+    print('Nom du user : ' + tweet.user.name)
+    print('Le tweet ID --> ' + str(tweet.id) + ' - Le tweet : ' + tweet.text)
+
+#Afficher les abonnés
+for follower in tweepy.Cursor(api.get_followers).items():
+    print("Followers : " + follower.name)
+
+print("Home tweets :")
+for tweet in tweepy.Cursor(api.home_timeline).items(10):
+    print(f"{tweet.user.name} tweet: {tweet.text}") 
+
+print('\nTweets avec hashtag : ')
+for tweet in tweepy.Cursor(api.search_tweets, q = QUERY).items():
+	try:
+        
+		print('\nTweet by: @' + tweet.user.screen_name)
+		tweet.retweet()
+		print('J''ai Retweet le tweet')
+
+		# Favorite the tweet
+		if LIKE:
+			tweet.favorite()
+			print('J''ai fav le tweet')
+
+		# Follow the user who tweeted
+		# check that bot is not already following the user
+		if FOLLOW:
+			if not tweet.user.following:
+				tweet.user.follow()
+				print('j''ai follow le compte ;)')
+
+		sleep(SLEEP_TIME)
+
+	except AttributeError  as e:
+		print(e.reason)
+
+	except StopIteration:
+		break
+
+
